a9225ed0d44906525f43c4d1b19d0e5c888c6631
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-02-20 15:28:07 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,4 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+# Create method `parrot` that outputs a given phrase and returns the phrase
 def parrot(word = 'Squawk!')
   puts word
   return word
